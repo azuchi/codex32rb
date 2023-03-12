174b30b4b7057b16a87a06941d07e42f14f28c0d
azuchi <azuchi@chaintope.com>
2023-03-12 11:06:11 +0900

    initial commit

---
+++ lib/codex32.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "codex32/version"
+
+module Codex32
+  class Error < StandardError
+  end
+  # Your code goes here...
+end
---
+++ spec/codex32_spec.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+RSpec.describe Codex32 do
+  it "has a version number" do
+    expect(Codex32::VERSION).not_to be nil
+  end
+end
